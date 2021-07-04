fccfd66b3106b7dc029a6fa547322c7e56097f53
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-04 13:27:54 +0900

    로그인, 로그아웃 기능 구현

--- src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -39,6 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+                .logoutUrl("/user/logout")
                 .permitAll();
     }
 }
